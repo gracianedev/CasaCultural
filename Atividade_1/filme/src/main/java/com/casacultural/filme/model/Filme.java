3f6458ff21c693c9f790eb5aa3bffac8f7b0b1e5
Graciane da Silva <graciane.dev@gmail.com>
2025-04-18 21:58:32 -0300

    Iniciado o desenvolvimento de View e corrigido um import na classe Filme (package model).

--- Atividade_1/filme/src/main/java/com/casacultural/filme/model/Filme.java
+++ Atividade_1/filme/src/main/java/com/casacultural/filme/model/Filme.java
@@ -6,7 +6,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Table;
 import lombok.Data;
-import org.springframework.data.annotation.Id;
+import jakarta.persistence.Id;
 import org.springframework.stereotype.Component;
 
 /**
