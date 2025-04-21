426056b302131014efec384a86c6d6e71c638c84
Graciane da Silva <graciane.dev@gmail.com>
2025-04-21 12:44:22 -0300

    Aplicados estilos nas Views. Iniciado o desenvolvimento das páginas analise.html e detalhe.html e adicionados seus mapeamentos em FilmeController.

--- Atividade_1/filme/src/main/java/com/casacultural/filme/controller/FilmeController.java
+++ Atividade_1/filme/src/main/java/com/casacultural/filme/controller/FilmeController.java
@@ -6,11 +6,14 @@ package com.casacultural.filme.controller;
 
 import com.casacultural.filme.model.Filme;
 import com.casacultural.filme.service.FilmeService;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 /**
@@ -41,8 +44,20 @@ public class FilmeController {
     }
     
     @GetMapping ("/lista")
-    public String lista () {
+    public String lista (Model model) {
+        model.addAttribute("filmes", filmeService.listarTodos());
         return "lista-filmes";
     }
     
-}
+    @GetMapping ("/analise")
+    public String avaliar (@PathVariable int id, Model model){
+        model.addAttribute("filme", filmeService.buscarPorId(id));
+        return "analise";
+    }
+    
+    @GetMapping ("/detalhe")
+    public String detalhe (@PathVariable int id, Model model){
+        model.addAttribute("filme", filmeService.buscarPorId(id));
+        return ("detalhe");
+    }
+} 
